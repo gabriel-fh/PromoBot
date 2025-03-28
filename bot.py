daafe2d604a38964d9c907f273f94359f84ce71e
Gabriel Hermenegildo <gabriel.f.hermenegildo@gmail.com>
2025-03-28 00:41:58 -0300

    feat: integrate web scraping with Discord bot to list products

--- bot.py
+++ bot.py
@@ -1,6 +1,31 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
 from scraper import Scrapper
 
-promotions = Scrapper().get_promotions()
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-for promotion in promotions:
-    print(promotion)
+intents = discord.Intents.default()
+intents.message_content = True 
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot {bot.user} está online!")
+
+@bot.command(name="promocoes")
+async def listar_promocoes(ctx):
+    promocoes = Scrapper().get_promotions() 
+    if isinstance(promocoes, str):
+        await ctx.send(promocoes) 
+    else:
+        if promocoes:
+            promo = promocoes[0] 
+            formatted_promo = f"**{promo['title']}**\nPreço: {promo['price']}\nMétodo de Pagamento: {promo['payment_method']}\nLink: {promo['link']}\nImagem: {promo['image']}\nCupom: {promo['coupon']}\n"
+            await ctx.send(formatted_promo)
+        else:
+            await ctx.send("Não foi possível encontrar promoções.")
+
+bot.run(TOKEN)
